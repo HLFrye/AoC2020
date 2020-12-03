f5a47a171141a176f2d5e8bcbb7b8e396654c487
Frye <hlfrye@gmail.com>
2020-12-03 00:34:48 -0600

    Day 2 Puzzle 2

--- day2/main.py
+++ day2/main.py
@@ -1,9 +1,21 @@
 from functools import partial
 
-def validate(min, max, key, test):
+def part1_validate(min, max, key, test):
     occs = test.count(key)
     if occs >= min and occs <= max:
         return True
+
+# 257 is too low?
+
+def validate(p1, p2, key, test):
+    isValid = 0
+    if test[p1-1] == key:
+        isValid += 1
+    if test[p2-1] == key:
+        isValid += 1
+    if isValid != 1:
+        print("{}-{} {}: {}".format(p1, p2, key, test))
+    return isValid == 1
 
 def validatorbuilder(rule):
     counts, key = rule.split(" ")
@@ -16,6 +28,7 @@ def main():
     with open("./input.txt") as f:
         for line in f.readlines():
             rule, test = line.split(": ")
+            test = test.strip()
             if rule not in validators:
                 validators[rule] = validatorbuilder(rule)
             validator = validators[rule]
